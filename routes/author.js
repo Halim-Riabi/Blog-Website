01fd1796e16205b2a2ff448fc6d98a19b25ac6a6
guyroW <halimriabi001@gmail.com>
2023-10-24 23:59:40 +0100

    register with upload image and crypt password

--- routes/author.js
+++ routes/author.js
@@ -2,10 +2,48 @@ const express = require('express');
 
 const router = express.Router();
 
-const Article = require('../models/author');
+const Author = require('../models/author');
 const multer = require('multer');
+const bcrypt = require('bcrypt');
 
-router.post('/register', (req, res)=>{
+filename='';
+const mystorage = multer.diskStorage({
+    destination : './uploads',
+    filename: (req, file, redirect)=>{
+        let date = Date.now();
+
+        let f1 = date + '.' + file.mimetype.split('/')[1];
+        //78788768688767.png
+        redirect(null , f1);
+        filename = f1;
+    }
+})
+
+const upload = multer({storage: mystorage})
+
+
+router.post('/register', upload.any('image'),(req, res)=>{
+
+    data = req.body;
+    author = new Author(data);
+
+    author.image= filename;
+    // cryptage password
+    salt = bcrypt.genSaltSync(10);
+    author.password = bcrypt.hashSync(data.password , salt);
+    
+    author.save()
+    .then(
+        (savedAuthur)=>{
+            filename="";
+            res.status(200).send(savedAuthor);
+        }
+    )
+    .catch(
+        err=>{
+            res.send(err)
+        }
+    )
 
     
 })
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const articleApi = require('./routes/article');
+const authorApi = require('./routes/author');
 require('./config/connect');
 
 
 const app = express();
 
 app.use('/article', articleApi);
+app.use('/author', authorApi);
 
 app.use('/getimage', express.static('.uploads'));
 
