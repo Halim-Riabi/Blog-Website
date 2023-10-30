c73c6c71b36d665a2d6b22c7b0fcd638ee0c55e0
guyroW <halimriabi001@gmail.com>
2023-10-30 20:26:21 +0100

    create all pages, routing, header and footer

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { AboutComponent } from './about/about.component';
+import { CreatearticleComponent } from './createarticle/createarticle.component';
+import { DetailComponent } from './detail/detail.component';
+import { HomeComponent } from './home/home.component';
+import { AuthorComponent } from './author/author.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , redirectTo: '/home' , pathMatch:'full'},
+  { path: 'home' , component: HomeComponent},
+  { path: 'article/:id' , component: DetailComponent},
+  { path: 'create' , component: CreatearticleComponent},
+  { path: 'about' , component: AboutComponent},
+  { path: 'privacy' , component: PrivacyComponent},
+  { path: 'author/:id' , component: AuthorComponent},
+  { path: 'login' , component: LoginComponent},
+  { path: 'register' , component: RegisterComponent},
+  { path: '**' , component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
